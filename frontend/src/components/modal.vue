9f939ad987ecc490a17a146c298fb28497c6ecd0
furia0928 <furia0928@gmail.com>
2020-06-30 20:33:14 +0900

    tt

---
+++ frontend/src/components/modal.vue
@@ -0,0 +1,58 @@
+<template
+  ><transition name="modal">
+    <div class="modal-mask">
+      <div class="modal-wrapper">
+        <div class="modal-container">
+          <!--<div class="modal-header">
+            <slot name="header">
+              default header
+            </slot>
+          </div>-->
+
+          <div class="modal-body">
+            <slot name="body">
+              <div v-swiper:mySwiper="swiperOption" @someSwiperEvent="callback">
+                <div class="swiper-wrapper">
+                  <div
+                    class="swiper-slide"
+                    v-for="(slide, index) in slideData"
+                    :key="index"
+                  >
+                    {{ slide.title }}
+                  </div>
+                </div>
+
+                <div class="swiper-button-prev" slot="button-prev"></div>
+                <div class="swiper-button-next" slot="button-next"></div>
+                <div class="swiper-pagination"></div>
+              </div>
+            </slot>
+          </div>
+
+          <button class="modal-default-button" @click="$emit('close')">
+            OK
+          </button>
+        </div>
+      </div>
+    </div>
+  </transition>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      swiperOption: {
+        initialSlide: this.openIndex,
+        pagination: {
+          el: ".swiper-pagination",
+        },
+        navigation: {
+          nextEl: ".swiper-button-next",
+          prevEl: ".swiper-button-prev",
+        },
+      },
+    };
+  },
+  props: ["slideData", "openIndex"],
+};
+</script>
--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,17 +1,26 @@
-import Vue from 'vue';
-import App from './App.vue';
-import router from './routes';
-import store from './store';
-import * as filters from './utils/filters.js';
-import axios from 'axios';
+import Vue from "vue";
+import ElementUI from "element-ui";
+import "element-ui/lib/theme-chalk/index.css";
+import App from "./App.vue";
+import router from "./routes";
+import store from "./store";
+import * as filters from "./utils/filters.js";
+import axios from "axios";
+import VueAwesomeSwiper from "vue-awesome-swiper";
+import "swiper/css/swiper.css";
+
+Vue.use(ElementUI);
+
+Vue.use(VueAwesomeSwiper /* { default options with global component } */);
+
 Vue.prototype.$http = axios;
 
 Vue.config.productionTip = false;
 
-Object.keys(filters).forEach(key => Vue.filter(key, filters[key]));
+Object.keys(filters).forEach((key) => Vue.filter(key, filters[key]));
 
 new Vue({
-	router,
-	store,
-	render: h => h(App),
-}).$mount('#app');
+  router,
+  store,
+  render: (h) => h(App),
+}).$mount("#app");
