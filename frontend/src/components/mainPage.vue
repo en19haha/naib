c848c5f90239b297bf6937f7579dbb0f1918268a
Seon <seonyeaji@gmail.com>
2020-07-11 09:39:24 +0900

    ASDASD

--- frontend/src/components/mainPage.vue
+++ frontend/src/components/mainPage.vue
@@ -1,14 +1,27 @@
 <template>
   <div class="section">
-    <ul class="nav-list">
-      <li><router-link to="/icondeco">decotter</router-link></li>
-      <li><router-link to="/message">message</router-link></li>
-      <li><router-link to="/collaboration">collaboration</router-link></li>
-    </ul>
-    <div class="guide-box">
-      <h1> 메인 요기 안에서 디자인 자유롭게<br>
-      높이 값은 상관없음</h1>
-    </div>
+	  <div class="inner">
+		  <h1 class="happy-title">
+			  <img src="../assets/images/title.png" alt="">
+		  </h1>
+		  <ul class="nav-list">
+			  <li class="decotter"><router-link to="/icondeco"><span>decotter</span></router-link></li>
+			  <li class="message"><router-link to="/message"><span>message</span></router-link></li>
+			  <li class="collaboration"><router-link to="/collaboration"><span>collaboration</span></router-link></li>
+		  </ul>
+		  <hr class="green-hr">
+		  <p class="main-desc">
+			  //<br><br>
+
+			  7월 23일, 나이브 수베다르의 생일을<br>
+			  축하하기 위한 페이지입니다.<br>
+			  생일 축하해, 나이브!<br><br>
+
+			  //
+		  </p>
+		  <hr class="green-hr">
+		  <a href="https://twitter.com/naib_723" target="_blank" class="link-twitter"><span>twitter</span></a>
+	  </div>
   </div>
 </template>
 <script>
