b2614dabd646e004a671d938389f319cc75dc6c8
Seon <seonyeaji@gmail.com>
2020-07-11 10:03:03 +0900

    ASDASD

--- frontend/src/components/decotter.vue
+++ frontend/src/components/decotter.vue
@@ -1,8 +1,23 @@
 <template>
 	<div class="section">
-		<div class="guide-box">
-			<h1>icondeco 요기 안에서 디자인 자유롭게<br>
-			높이 값은 상관없음</h1>
+		<div class="inner">
+			<h1 class="happy-title">
+				<img src="../assets/images/decotter_title.png" alt="">
+			</h1>
+			<a href="https://icondecotter.jp/detail.php?id=60279" target="_blank" class="decotter-link"><span>decotter twitter</span></a>
+			<hr class="green-hr mt77">
+			<p class="main-desc">
+				//<br><br>
+
+				생일 기념 아이콘 테두리를 배포합니다.<br>
+				트위터 아이콘을 장식해 나이브의 생일을 축하해 주세요!<br><br>
+
+				//
+			</p>
+			<hr class="green-hr">
+			<div class="decotter-img">
+				<img src="../assets/images/decotter.jpg" alt="">
+			</div>
 		</div>
 	</div>
 </template>
