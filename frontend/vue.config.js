fd2b4bc83f20809c68356b512fe36603f34bb734
furia0928 <furia0928@gmail.com>
2020-07-13 14:09:33 +0900

    dd

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -1,6 +1,10 @@
 var path = require("path");
+const ansiRegex = require("ansi-regex");
 module.exports = {
+  lintOnSave: false,
+  assetsDir: process.env.BASE_URL,
   outputDir: path.resolve(__dirname, "../backend/src/public/"),
+  transpileDependencies: [ansiRegex],
   devServer: {
     overlay: false,
   },
