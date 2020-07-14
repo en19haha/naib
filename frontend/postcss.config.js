cd3faf38810205ae885f6d7c7e966dfd589ef720
furia0928 <furia0928@gmail.com>
2020-07-14 15:17:28 +0900

    dd

--- frontend/postcss.config.js
+++ frontend/postcss.config.js
@@ -1,9 +1,6 @@
 module.exports = {
   plugins: [
-    /*require('postcss-import')({
-            path: ['app/assets/'],
-        }),*/
-
+    require("postcss-minify-font-values"),
     require("postcss-flexbugs-fixes"),
     require("autoprefixer")({
       browsers: ["last 2 versions"],
