adf8f1246513053a93d39850d5adc82838db3e18
Seon <seonyeaji@gmail.com>
2020-07-20 23:36:25 +0900

    d

--- frontend/src/components/mainPage.vue
+++ frontend/src/components/mainPage.vue
@@ -14,12 +14,16 @@
                 </ul>
                 <hr class="green-hr" />
                 <p class="main-desc">
-                    //<br /><br />
+	                //<br /><br />
 
-                    7월 23일, 나이브 수베다르의 생일을<br />
-                    축하하기 위한 페이지입니다.<br /><br />
+	                7월 23일, 나이브 수베다르의 생일을<br />
+	                축하하기 위한 페이지입니다.<br />
+	                생일 축하해, 나이브!<br />
 
-                    //
+	                //<br /><br />
+
+	                해당 사이트는 PC로의 접속을 권장하며<br />
+	                Google Chrome과 Microsoft Edge에 최적화 되어있습니다.
                 </p>
                 <hr class="green-hr mt45" />
                 <a href="https://twitter.com/naib_723" target="_blank" class="link-twitter"
